8ce8aba16bcbe591b115da55801afdca719b5376
Iago <iagocesarts@hotmail.com>
2024-11-23 20:49:59 -0300

    feature: terminado a aplicação

--- .build.ps1
+++ .build.ps1
@@ -5,3 +5,25 @@ task compile {
 task run {
     java -cp "src/class;src/lib/jackson-core-2.15.2.jar;src/lib/jackson-databind-2.15.2.jar" Main
 }
+
+task jar {
+    $manifest = "Manifest.txt"
+    Set-Content -Path $manifest -Value "Main-Class: Main`n"
+    jar cvfm dorks.jar $manifest -C src/class . -C src/lib .
+    Remove-Item $manifest
+}
+
+task test {
+    $threads = @(1, 5, 10, 20)
+    $dorksFiles = @("src/tests/dorks_20.txt", "src/tests/dorks_50.txt", "src/tests/dorks_80.txt")
+    $tokensFiles = @("src/tests/token_1.txt", "src/tests/token_2.txt", "src/tests/token_3.txt")
+
+    foreach ($dorks in $dorksFiles) {
+        foreach ($tokens in $tokensFiles) {
+            foreach ($thread in $threads) {
+                Write-Output "Executando com $thread threads, dorks: $dorks, tokens: $tokens"
+                java -jar dorks.jar -d $dorks -t $tokens -n $thread | Select-Object -Last 5 | Out-File -Append -FilePath "Resultado_teste.txt"
+            }
+        }
+    }
+}
--- makefile
+++ makefile
@@ -11,6 +11,21 @@ compile: ./src/java/CredentialManager.java ./src/java/GitHubSearchTask.java ./sr
 run: ./src/class/Main.class ./src/lib/jackson-core-2.15.2.jar
 	java -cp "src/class:src/lib/jackson-core-2.15.2.jar:src/lib/jackson-databind-2.15.2.jar" Main
 
+jar: ./src/class/Main.class ./src/class/CredentialManager.class ./src/class/GitHubSearchTask.class ./src/class/Token.class
+	jar cvfm dorks.jar -C src/class . -C src/lib .
+
+test: jar
+	@threads="1 5 10 20"; \
+	dorksFiles="src/tests/dorks_20.txt src/tests/dorks_50.txt src/tests/dorks_80.txt"; \
+	tokensFiles="src/tests/token_1.txt src/tests/token_2.txt src/tests/token_3.txt"; \
+	for dorks in $$dorksFiles; do \
+		for tokens in $$tokensFiles; do \
+			for thread in $$threads; do \
+				echo "Executando com $$thread threads, dorks: $$dorks, tokens: $$tokens"; \
+				java -jar dorks.jar -d $$dorks -t $$tokens -n $$thread | tail -n 5 >> Resultado_teste.txt; \
+			done; \
+		done; \
+	done
 
 %:
 	@:
