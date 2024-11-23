6c76952f37312ef7665a95b13156840553ff460a
Iago <iagocesarts@hotmail.com>
2024-11-23 19:43:47 -0300

    refactor: organiza os arquivos novamente e gera os testes no powershell

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 THREADS=(1 5 10 20)
-DORKS_FILES=("Dorks/dorks_20.txt" "Dorks/dorks_50.txt" "Dorks/dorks_80.txt")
-TOKENS_FILES=("Tokens/tokens_1.txt" "Tokens/tokens_2.txt" "Tokens/tokens_3.txt")
+DORKS_FILES=("tests/dorks_20.txt" "tests/dorks_50.txt" "tests/dorks_80.txt")
+TOKENS_FILES=("tests/token_1.txt" "tests/token_2.txt" "tests/token_3.txt")
 
 for dorks in "${DORKS_FILES[@]}"; do
   for tokens in "${TOKENS_FILES[@]}"; do
